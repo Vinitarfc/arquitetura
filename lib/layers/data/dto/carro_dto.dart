afab5bb9d9202fa0ed32ba966a5dac8755c16f1c
vinicius.machado <vinitarfc@gmail.com>
2023-12-07 11:09:29 -0300

    CLEAN ARCHITECTURE: DTO PATTERN e DATA LAYER

---
+++ lib/layers/data/dto/carro_dto.dart
@@ -0,0 +1,28 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:arquitetura/layers/domain/entities/carro_entity.dart';
+
+class CarroDto extends CarroEntity {
+  String placa;
+  int quantidadeDePortas;
+  double valorFinal;
+  CarroDto({
+    required this.placa,
+    required this.quantidadeDePortas,
+    required this.valorFinal,
+  }) : super(placa: placa, qtdePortas: quantidadeDePortas, valor: valorFinal);
+
+  Map toMap() {
+    return {
+      'placa': this.placa,
+      'quantidadeDePortas': this.quantidadeDePortas,
+      'valorFinal': this.valorFinal
+    };
+  }
+
+  CarroDto fromMap(Map map) {
+    return CarroDto(
+        placa: map['placa'],
+        quantidadeDePortas: map['quantidadeDePortas'],
+        valorFinal: map['valorFinal']);
+  }
+}
